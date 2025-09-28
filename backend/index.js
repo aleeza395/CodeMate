cd77672567e6eac0456c7e513fdde024a7926a83
aleeza395 <aliza25300@gmail.com>
2025-09-28 05:38:28 -0700

    updated index

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
 import express from 'express'
+const app = express()
 import dotenv from 'dotenv'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
 import signuproute from './routes/signupform.js'
 import loginroute from './routes/loginform.js'
 import queryroute from './routes/queryroute.js'
@@ -12,20 +10,21 @@ import updatesroute from './routes/updatesroute.js'
 import runcode from './routes/runcode.js'
 import checkcode from './routes/checkcode.js'
 import quizroute from './routes/quizroute.js'
-import quizResultsRoutes from "./routes/quizresult.js"
+import quizResultsRoutes from "./routes/quizresult.js";
 import missionroute from "./routes/missionroute.js"
-
-dotenv.config()
-
-const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("Database connected"))
-  .catch(err => console.log("Error connecting to database:", err))
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("Database connected"))
+.catch(err => console.log("Error connecting to database"))
+
+app.get('/', (req, res) => {
+    res.send("Backend is running ")
+})
 
 app.use('/signup', signuproute)
 app.use('/login', loginroute)
@@ -34,18 +33,8 @@ app.use('/updates', updatesroute)
 app.use('/runcode', runcode)
 app.use('/checkcode', checkcode)
 app.use('/quizroute', quizroute)
-app.use("/api/quizresults", quizResultsRoutes)
+app.use("/api/quizresults", quizResultsRoutes);
 app.use("/api/missions", missionroute)
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
-app.use(express.static(path.join(__dirname, "frontend/build")))
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"))
-})
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT)
